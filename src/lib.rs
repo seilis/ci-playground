4657c4b982bb6d68c6148775d85b98af1cc0c992
Aaron Seilis <aaron.seilis@seilis.ca>
2021-10-03 10:51:08 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,32 @@
+
+/// This function adds
+///
+/// # Examples
+/// ```
+/// use ci_testing::add;
+/// assert_eq!(add(2,2), 4) ;
+/// ```
+pub fn add(x: u32, y: u32) -> u32 {
+   x + y 
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+
+    #[test]
+    fn test_add() {
+        assert_eq!(add(2,2), 4);
+    }
+
+    #[test]
+    fn test_add2() {
+        assert_eq!(add(2,2), 4, "I messed up adding");
+    }
+}
